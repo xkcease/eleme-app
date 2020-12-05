08335c99721c56b5fe2834a8ea33244afa9ad838
xkcease <xkcease@163.com>
2020-12-05 10:30:35 +0800

    feat(Goods): add goods page

---
+++ src/components/Wrap.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="wrap">
+        <slot></slot>
+    </div>
+</template>
+
+
+<script>
+    export default {
+
+    };
+</script>
+
+
+<style lang="scss">
+    .wrap {
+        display: flex;
+        width: 100%;
+        min-height: 300px;
+        position: absolute;
+        top: 179px;
+        bottom: 0;
+        overflow: hidden;
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,58 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+    <div class="home">
+        <Topbar :sellerProp="sellerProp" />
+        <router-view />
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+    import Topbar from "../components/header/Topbar";
+    import axios from "axios";
+    import { mapMutations } from 'vuex';
 
-export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
-};
+    export default {
+        name: "Home",
+        components: {
+            Topbar,
+        },
+        data() {
+            return {
+                ratingsProp: [],
+                sellerProp: {},
+            };
+        },
+        methods: {
+            ...mapMutations(['updateDeliveryPrice', 'updateMinPrice'])
+        },
+        beforeRouteEnter(to, from, next) {
+            function getRatings() {
+                return axios.get("/api/ratings");
+            }
+
+            function getSeller() {
+                return axios.get("/api/seller");
+            }
+
+            Promise.all([getRatings(), getSeller()]).then(
+                ([ratingsData, sellerData]) => {
+                    next((vm) => {
+                        vm.ratingsProp = ratingsData.data;
+                        vm.sellerProp = sellerData.data;
+                        vm.updateDeliveryPrice({ deliveryPrice: sellerData.data.deliveryPrice });
+                        vm.updateMinPrice({ minPrice: sellerData.data.minPrice });
+                    });
+                }
+            );
+        },
+    };
 </script>
+
+<style lang="scss">
+    .home {
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 53px;
+    }
+</style>
