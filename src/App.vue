7a16060a3687d6b42556058179d3b0f0ec80a21d
xkcease <xkcease@163.com>
2020-12-09 19:18:34 +0800

    modify App and main.js

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,70 @@
 <template>
     <div id="app">
-        <router-view />
+        <Topbar :sellerProp="sellerProp" :supportsClassMap="supportsClassMap" />
+        <transition name="tab-slide">
+            <router-view />
+        </transition>
         <Cart />
     </div>
 </template>
 
 <script>
+import './assets/css/icon.css';
 import Cart from './components/Cart';
+import Topbar from './components/header/Topbar';
+import axios from 'axios';
+import { mapMutations } from 'vuex';
 
 export default {
     components: {
         Cart,
+        Topbar,
+    },
+    data() {
+        return {
+            ratingsProp: [],
+            sellerProp: {},
+            supportsClassMap: [
+                'decrease',
+                'discount',
+                'special',
+                'invoice',
+                'guarantee',
+            ],
+        };
+    },
+    methods: {
+        ...mapMutations([
+            'updateDeliveryPrice',
+            'updateMinPrice',
+            'updateSeller',
+        ]),
+    },
+    mounted() {
+        axios.get('/api/seller').then(res => {
+            this.sellerProp = res.data;
+            this.updateDeliveryPrice({
+                deliveryPrice: this.sellerProp.deliveryPrice,
+            });
+            this.updateMinPrice({ minPrice: this.sellerProp.minPrice });
+            this.updateSeller({ seller: this.sellerProp });
+        });
+
+        axios.get('/api/ratings').then(res => {
+            this.ratingsProp = res.data;
+        });
     },
 };
 </script>
 
 <style lang="scss">
+.tab-slide-enter-active,
+.tab-slide-leave-active {
+    transition: all 0.2s linear;
+}
+
+.tab-slide-enter,
+.tab-slide-leave-to {
+    transform: translateX(100%);
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,18 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import { Row, Col, Button, Sidebar, SidebarItem, Card, Badge } from 'vant';
+import {
+    Row,
+    Col,
+    Button,
+    Sidebar,
+    SidebarItem,
+    Card,
+    Badge,
+    Popup,
+    Cell,
+    CellGroup,
+} from 'vant';
 
 Vue.use(Row);
 Vue.use(Col);
@@ -11,11 +22,14 @@ Vue.use(Sidebar);
 Vue.use(SidebarItem);
 Vue.use(Card);
 Vue.use(Badge);
+Vue.use(Popup);
+Vue.use(Cell);
+Vue.use(CellGroup);
 
 Vue.config.productionTip = false;
 
 new Vue({
     router,
     store,
-    render: h => h(App)
+    render: h => h(App),
 }).$mount('#app');
