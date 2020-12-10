e7835c0c6511987357e93cad69b2e8425bc93e08
xkcease <xkcease@163.com>
2020-12-10 16:22:03 +0800

    feat: block component

---
+++ src/components/ContentBlock.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="content-block border-1px--top border-1px">
+        <h2 class="content-block__title" v-if="title">{{ title }}</h2>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ContentBlock',
+    props: {
+        title: String,
+    },
+};
+</script>
+
+<style lang="scss">
+.content-block {
+    background-color: #fff;
+    padding: 18px;
+    margin-bottom: 12px;
+}
+
+.content-block__title {
+    font-size: $md-size;
+    font-weight: 200;
+    margin-bottom: 12px;
+}
+</style>
